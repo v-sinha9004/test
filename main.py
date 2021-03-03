0d94254652b2cebfa6c3c8237b2ae0dd8deb6335
Vishal Sinha <vishal.s@smarter.codes>
2021-03-03 11:30:09 +0530

    add a meaning print statement

--- main.py
+++ main.py
@@ -3,34 +3,30 @@
 # python program to illustrate nested If statement 
 #!/usr/bin/python 
 i = 10
-if (i == 10): 
-    #  First if statement 
+if (i == 10):
+    print("1st level if") 
     if (i < 15): 
-        print ("i is smaller than 15") 
-    # Nested - if statement 
-    # Will only be executed if statement above 
-    # it is true 
+        print ("2nd level if") 
     if (i < 12): 
-        print ("i is smaller than 12 too")
+        print ("2nd level if")
         if (i < 12):
             print("3rd level if")
-        
             if (i < 12): 
-                print ("i is smaller than 12 too")
+                print ("4th level if")
                 if (i < 12):
-                    print("3rd level if")
+                    print("5rd level if")
                 else:
-                    print("3rd level else")
+                    print("5rd level else")
                     if (i < 12):
-                        print("4th level if")
+                        print("6th level if")
                         if (i < 12):
-                            print("5th level if")
+                            print("7th level if")
                         else:
-                            print("5th level else")
+                            print("7th level else")
                     else:
-                        print("4th level else")
+                        print("6th level else")
             else: 
-                print ("i is greater than 15")
+                print ("4th level else")
         else:
             print("3rd level else")
             if (i < 12):
@@ -42,6 +38,6 @@ if (i == 10):
             else:
                 print("4th level else")
     else: 
-        print ("i is greater than 15")
+        print ("2nd level else")
 else:
-    print("First nesting")
+    print("1st level else")
